41c5eed253132f73be9e7c6349b43b25948810f1
Santhosh <dasarisanthosh8688@gmail.com>
2025-06-19 11:30:17 +0530

    indext.html

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let resultElement = document.getElementById('result');
 
-function leeLa(value) {
+function santhosh(value) {
     resultElement.innerText += value;
+    
+}
+function clearDisplay(){
+    resultElement.innerText = "0"
 }
 function equal(){
     let result = eval(resultElement.innerText);
     resultElement.innerText = result;
 }
+ function backspace(){
+    resultElement.innerText = resultElement.innerText.slice(0,1);
+ }
